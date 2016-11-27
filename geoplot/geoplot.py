236723f18e3abad39cefb4c1a2f001d14eaafe71
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2016-11-26 22:31:08 -0500

    Stop cartopy work for now, let's try rasterio...

--- geoplot/geoplot.py
+++ geoplot/geoplot.py
@@ -3,6 +3,7 @@ import pandas as pd
 from shapely import geometry
 import matplotlib.pyplot as plt
 import numpy as np
+from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 
 
@@ -19,7 +20,6 @@ def pointplot(df,
     xs = np.array([p.x for p in df.geometry])
     ys = np.array([p.y for p in df.geometry])
 
-    # Handle the projection.
     # If we are not handed a projection we are in the PateCarree projection. In that case we can return a
     # `matplotlib` plot directly, which has the advantage of being native to e.g. mplleaflet.
     if not projection:
@@ -54,7 +54,7 @@ def pointplot(df,
     #
     # For now this is all a massive "to-do". I'm not sure what the most appealing API for this would be,
     # given the limitations present.
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     if ('lon_0' in proj_params.keys() and proj_params['lon_0'] == 0) or\
        ('lat_0' in proj_params.keys() and proj_params['lat_0'] == 0):
         projkwargs = dict()
@@ -71,7 +71,9 @@ def pointplot(df,
         print({**proj_params, **projkwargs})
         projection = projection.__class__(**projkwargs, globe=globe)
 
-    # Set up the axis...
+    # Set up the axis. Note that even though the method signature is from matplotlib, after this operation ax is a
+    # cartopy.mpl.geoaxes.GeoAxesSubplot object! This is a subclass of a matplotlib Axes class but not directly
+    # compatible with one, so it means that this axis cannot, for example, be plotted using mplleaflet.
     ax = plt.subplot(111, projection=projection)
 
     # Set optional params.
@@ -82,6 +84,85 @@ def pointplot(df,
     if coastlines:
         ax.coastlines()
 
-    # Draw.
+    # Draw. Notice that this scatter method's signature is attached to the axis instead of to the overall plot. This
+    # is again because the axis is a special cartopy object.
     ax.scatter(xs, ys, transform=ccrs.PlateCarree(), **kwargs)
     plt.show()
+
+
+def choropleth(df,
+               data=None,
+               extent=None,
+               stock_image=False, coastlines=False,
+               projection=None,
+               figsize=(12, 4),
+               **kwargs):
+
+    # Initialize the figure.
+    fig = plt.figure(figsize=figsize)
+
+    # If we are not handed a projection we are in the PateCarree projection. In that case we can return a
+    # `matplotlib` plot directly, which has the advantage of being native to e.g. mplleaflet.
+    if not projection:
+        pass
+
+    # Otherwise, assemble the defaults.
+    proj_params = dict(projection.proj4_params)
+    globe = projection.globe
+
+    if ('lon_0' in proj_params.keys() and proj_params['lon_0'] == 0) or\
+       ('lat_0' in proj_params.keys() and proj_params['lat_0'] == 0):
+        xs = np.array([p.x for p in df.geometry.centroid])
+        ys = np.array([p.y for p in df.geometry.centroid])
+        projkwargs = dict()
+        if 'lon_0' in proj_params.keys() and proj_params['lon_0'] == 0:
+            projkwargs['central_longitude'] = np.mean(xs)
+            proj_params.pop('lon_0')
+        else:
+            pass
+        if 'lat_0' in proj_params.keys() and proj_params['lat_0'] == 0:
+            projkwargs['central_latitude'] = np.mean(ys)
+            proj_params.pop('lat_0')
+        else:
+            pass
+        projection = projection.__class__(**projkwargs, globe=globe)
+
+    # Initialize the cartopy axis.
+    ax = plt.subplot(111, projection=projection)
+
+    # Set optional params.
+    if extent:
+        ax.set_extent(extent)
+    else:
+        envelopes = df.geometry.envelope.exterior
+        import pdb; pdb.set_trace()
+        xmin = np.min(envelopes.map(lambda linearring: np.min([linearring.coords[1][0],
+                                                               linearring.coords[2][0],
+                                                               linearring.coords[3][0],
+                                                               linearring.coords[4][0]])))
+        ymin = np.min(envelopes.map(lambda linearring: np.min([linearring.coords[1][1],
+                                                               linearring.coords[2][1],
+                                                               linearring.coords[3][1],
+                                                               linearring.coords[4][1]])))
+        xmax = np.max(envelopes.map(lambda linearring: np.max([linearring.coords[1][0],
+                                                               linearring.coords[2][0],
+                                                               linearring.coords[3][0],
+                                                               linearring.coords[4][0]])))
+        ymax = np.max(envelopes.map(lambda linearring: np.max([linearring.coords[1][1],
+                                                               linearring.coords[2][1],
+                                                               linearring.coords[3][1],
+                                                               linearring.coords[4][1]])))
+        ax.set_extent((xmin, xmax, ymin, ymax))
+    if stock_image:
+        ax.stock_img()
+    if coastlines:
+        ax.coastlines()
+
+    # Draw. To do this, we'll have to bastardize parts of the geopandas drawing API, taking code from here:
+    # https://github.com/geopandas/geopandas/blob/46e50fe5a772944b325bc3c8806f4f96da76a0d8/geopandas/plotting.py#L120
+    # TODO: Implement this.
+    features = ShapelyFeature(df.geometry, ccrs.PlateCarree())
+    ax.add_feature(features, **kwargs)
+    plt.show()
+
+    pass
