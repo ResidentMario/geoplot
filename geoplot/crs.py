d677a67b3ca6cb771be2c86126ac808ab4177726
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2016-11-28 22:37:46 -0500

    More CRS work.

--- geoplot/crs.py
+++ geoplot/crs.py
@@ -1,162 +1,159 @@
 import cartopy.crs as ccrs
-import numpy as np
-
 
 # TODO: RotatedPole
 
 class PlateCarree:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class LambertCylindrical:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Mercator:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Miller:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Mollweide:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Robinson:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Sinusoidal:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class InterruptedGoodeHomolosine:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Geostationary:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class NorthPolarStereo:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class SouthPolarStereo:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_longitude': centerings['central_longitude']})
 
 
 class Gnomonic:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_latitude=True)
+    def load(self, df, centerings):
+        return _generic_load(self, df, {'central_latitude': centerings['central_latitude']})
 
 
 class AlbersEqualArea:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class AzimuthalEquidistant:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class LambertConformal:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class Orthographic:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class Stereographic:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class TransverseMercator:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class LambertAzimuthalEqualArea:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df, has_central_longitude=True, has_central_latitude=True)
+    def load(self, df, centerings): return _generic_load(self, df, centerings)
 
 
 class UTM:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df)
+    def load(self, df, _): return _generic_load(self, df, dict())
 
 
 class OSGB:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df)
+    def load(self, df, _): return _generic_load(self, df, dict())
 
 
 class EuroPP:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df)
+    def load(self, df, _): return _generic_load(self, df, dict())
 
 
 class OSNI:
     def __init__(self, **kwargs): self.args = kwargs
 
-    def load(self, df): return _generic_load(self, df)
+    def load(self, df, _): return _generic_load(self, df, dict())
 
-def _generic_load(proj, df,
-                  has_central_longitude=False, has_central_latitude=False):
+
+def _generic_load(proj, df, centerings):
     centering_variables = dict()
-    if has_central_longitude:
-        if "central_longitude" not in proj.args.keys():
-            central_longitude = np.mean(np.array([p.x for p in df.geometry]))
-        else:
-            central_longitude = proj.args['central_longitude']
-            proj.args.pop('central_longitude')
-        centering_variables['central_longitude'] = central_longitude
-    if has_central_latitude:
-        if "central_latitude" not in proj.args.keys():
-            central_latitude = np.mean(np.array([p.x for p in df.geometry]))
-        else:
-            central_latitude = proj.args['central_latitude']
-            proj.args.pop('central_latitude')
-        centering_variables['central_latitude'] = central_latitude
-    import pdb; pdb.set_trace()
+    for key, func in centerings.items():
+        centering_variables[key] = func(df)
     return getattr(ccrs, proj.__class__.__name__)(**{**centering_variables, **proj.args})
