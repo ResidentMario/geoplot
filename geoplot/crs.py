98292accc6f4cadbab9ada93564d06c3984fe1f8
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2016-11-29 21:45:55 -0500

    going to have to scrap this...

--- geoplot/crs.py
+++ geoplot/crs.py
@@ -1,3 +1,21 @@
+# All of the optional parameters passed to the Cartopy CRS instance as an argument to the projection parameter
+# above are themselves passed on, via initialization, into a `proj4_params` attribute of the class, which is a
+# basic dict of the form e.g.:
+# >>> projection.proj4_params
+# <<< {'proj': 'eqc', 'lon_0': 0.0, 'a': 57.29577951308232, 'ellps': 'WGS84'}
+#
+# In general Python follows the philosophy that everything should be mutable. This object, however,
+# refuses assignment. For example witness what happens when you insert the following code:
+# >>> projection.proj4_params['a'] = 0
+# >>> print(projection.proj4_params['a'])
+# <<< 57.29577951308232
+# In other words, Cartopy CRS internals are immutable; they can only be set at initialization.
+# cf. http://stackoverflow.com/questions/40822241/seemingly-immutable-dict-in-object-instance/40822473
+#
+# I tried several workarounds. The one which works best is having the user pass a geoplot.crs.* to projection;
+# the contents of geoplot.crs are a bunch of thin projection class wrappers with a factory method, "load",
+# for properly configuring a Cartopy projection with or without optional central coordinate(s).
+
 import cartopy.crs as ccrs
 
 # TODO: RotatedPole
