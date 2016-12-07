dccee286fc6f5a13f6d99639c480a81504bcc8de
ResidentMario <aleksey.bilogur@gmail.com>
2016-12-06 19:14:19 -0500

    Initial draft of docs.

---
+++ docs/geoplot.rst
@@ -0,0 +1,38 @@
+geoplot package
+===============
+
+Submodules
+----------
+
+geoplot.crs module
+------------------
+
+.. automodule:: geoplot.crs
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+geoplot.geoplot module
+----------------------
+
+.. automodule:: geoplot.geoplot
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+geoplot.quad module
+-------------------
+
+.. automodule:: geoplot.quad
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: geoplot
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- geoplot/crs.py
+++ geoplot/crs.py
@@ -2,25 +2,8 @@
 This module defines the ``geoplot`` coordinate reference system classes, wrappers on ``cartopy.crs`` objects meant
 to be used as parameters to the ``projection`` parameter of all front-end ``geoplot`` outputs.
 
-This was necessary because ``cartopy.crs`` objects do not allow modifications in place. Allow me to explain:
-
-All of the optional parameters passed to the Cartopy CRS instance as an argument to the projection parameter
-above are themselves passed on, via initialization, into a `proj4_params` attribute of the class, which is a
-basic dict of the form e.g.:
-# >>> projection.proj4_params
-# <<< {'proj': 'eqc', 'lon_0': 0.0, 'a': 57.29577951308232, 'ellps': 'WGS84'}
-
-In general Python follows the philosophy that everything should be mutable. This object, however,
-refuses assignment. For example witness what happens when you insert the following code:
-# >>> projection.proj4_params['a'] = 0
-# >>> print(projection.proj4_params['a'])
-# <<< 57.29577951308232
-In other words, Cartopy CRS internals are immutable; they can only be set at initialization.
-cf. http://stackoverflow.com/questions/40822241/seemingly-immutable-dict-in-object-instance/40822473
-
-I tried several workarounds. The one which works best is having the user pass a geoplot.crs.* to projection;
-the contents of geoplot.crs are a bunch of thin projection class wrappers with a factory method, "load",
-for properly configuring a Cartopy projection with or without optional central coordinate(s). Hence this module.
+This was necessary because ``cartopy.crs`` objects do not allow modifications in place. cf.
+http://stackoverflow.com/questions/40822241/seemingly-immutable-dict-in-object-instance/40822473
 
 For the list of Cartopy CRS objects this module derives from, refer to
 http://scitools.org.uk/cartopy/docs/latest/crs/projections.html
@@ -182,7 +165,7 @@ class OSNI:
 
 def _generic_load(proj, df, centerings):
     """
-    A moderately mind-bendy metamethod which abstracts the internals of individual projections' load procedures.
+    A moderately mind-bendy meta-method which abstracts the internals of individual projections' load procedures.
 
     Parameters
     ----------
