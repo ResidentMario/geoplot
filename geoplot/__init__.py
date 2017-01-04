bb55eee0b3d0b5726a7fc9f3d30d5a84b0600d9f
ResidentMario <aleksey.bilogur@gmail.com>
2017-01-04 00:53:39 -0500

    Avoid exporting imports in init.

--- geoplot/__init__.py
+++ geoplot/__init__.py
@@ -1,4 +1,8 @@
-from .geoplot import *
-from .quad import *
-from .crs import *
-from .utils import *
+from .geoplot import pointplot, polyplot, choropleth, aggplot, cartogram, kdeplot, sankey
+from .quad import QuadTree, subpartition, flatten
+from .crs import (PlateCarree, LambertCylindrical, Mercator, Miller, Mollweide, Robinson, Sinusoidal,
+                  InterruptedGoodeHomolosine, Geostationary, NorthPolarStereo, SouthPolarStereo, Gnomonic,
+                  AlbersEqualArea, AzimuthalEquidistant, LambertConformal, Orthographic, Stereographic,
+                  TransverseMercator, LambertAzimuthalEqualArea, UTM, OSGB, EuroPP, OSNI)
+from .utils import (gaussian_points, classify_clusters, gaussian_polygons, gaussian_multi_polygons,
+                    uniform_random_global_points, uniform_random_global_network)
