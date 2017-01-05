0085ff70ca1c78dab6bedddb8ae373cf6d98175a
ResidentMario <aleksey.bilogur@gmail.com>
2017-01-04 20:05:38 -0500

    Update installation, installation instructions> .

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name = 'geoplot',
+    packages = ['geoplot'], # this must be the same as the name above
+    install_requires=['matplotlib', 'seaborn', 'pandas', 'geopandas', 'cartopy'],
+    py_modules=['geoplot', 'crs', 'utils', 'quad'],
+    version = '0.0.2',
+    description = 'High-level geospatial plotting for Python.',
+    author = 'Aleksey Bilogur',
+    author_email = 'aleksey.bilogur@gmail.com',
+    url = 'https://github.com/ResidentMario/geoplot',
+    download_url = 'https://github.com/ResidentMario/geoplot/tarball/0.0.2',
+    keywords = ['data', 'data visualization', 'data analysis', 'data science', 'pandas', 'geospatial data',
+                'geospatial analytics'],
+    classifiers = [],
+)
