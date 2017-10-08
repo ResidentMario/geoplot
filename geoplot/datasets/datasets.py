7e577adc6ac8d9bd940ee6e0530916ddd4291849
ResidentMario <aleksey.bilogur@gmail.com>
2017-10-08 02:42:20 -0400

    Correct dataset access.

--- geoplot/datasets/datasets.py
+++ geoplot/datasets/datasets.py
@@ -5,6 +5,7 @@ User-facing utility module for downloading example datasets. Similar to the `geo
 import geopandas as gpd
 import fiona
 from zipfile import ZipFile
+from pkg_resources import resource_stream, resource_listdir
 
 
 def load(dname):
@@ -21,7 +22,9 @@ def load(dname):
     GeoDataFrame instance
         The dataset being referenced.
     """
-    z = ZipFile("examples.zip")
+    assert 'examples.zip' in resource_listdir('geoplot.datasets', '')
+
+    z = ZipFile(resource_stream('geoplot.datasets', 'examples.zip'))
 
     with fiona.BytesCollection(z.read('geoplot-data/{0}.geojson'.format(dname))) as f:
         crs = f.crs
