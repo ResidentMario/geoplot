96c90f9096af115abefa852fef438a41c2267279
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-10-11 15:46:37 -0400

    Remove datasets submodule as it has been eclipsed.

--- geoplot/__init__.py
+++ geoplot/__init__.py
@@ -1,4 +1,3 @@
 from .geoplot import (pointplot, polyplot, choropleth, aggplot, cartogram, kdeplot, sankey)
 import geoplot.crs as crs
 import geoplot.utils as utils
-import geoplot.datasets as datasets
