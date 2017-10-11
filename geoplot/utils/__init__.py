a059990d9b0898c0fb588068ff631bd69b14e685
ResidentMario <aleksey.bilogur@gmail.com>
2017-10-11 18:00:27 -0400

    Reference figures short of collisions.

--- geoplot/utils/__init__.py
+++ geoplot/utils/__init__.py
@@ -1,2 +1,2 @@
 from .utils import (gaussian_points, gaussian_polygons, gaussian_multi_polygons, uniform_random_global_points,
-                    uniform_random_global_network)
+                    uniform_random_global_network, classify_clusters)
