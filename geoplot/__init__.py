f05531cdec67e0642a47e852773fd714ca71b7a7
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-10-14 22:02:02 -0400

    Another correction.

--- geoplot/__init__.py
+++ geoplot/__init__.py
@@ -1,3 +1,8 @@
 from .geoplot import (pointplot, polyplot, choropleth, aggplot, cartogram, kdeplot, sankey)
-from .crs import crs
-from .utils import utils
+from .crs import (PlateCarree, LambertCylindrical, Mercator, Miller, Mollweide, Robinson, Sinusoidal,
+                  InterruptedGoodeHomolosine, Geostationary, NorthPolarStereo, SouthPolarStereo, Gnomonic,
+                  AlbersEqualArea, AzimuthalEquidistant, LambertConformal, Orthographic, Stereographic,
+                  TransverseMercator, LambertAzimuthalEqualArea, UTM, OSGB, EuroPP, OSNI)
+from .utils import (gaussian_points, gaussian_polygons, gaussian_multi_polygons, uniform_random_global_points,
+                    uniform_random_global_network, classify_clusters)
+from .quad import (QuadTree, subpartition, flatten)
