8e51e1d433cafd2ee5483ef7dda3083f67b4d54c
ResidentMario <aleksey.bilogur@gmail.com>
2019-03-16 19:59:33 -0700

    Explicitly depend on descartes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='geoplot',
     packages=['geoplot'], # this must be the same as the name above
-    install_requires=['matplotlib', 'seaborn', 'pandas', 'geopandas', 'cartopy'],
+    install_requires=['matplotlib', 'seaborn', 'pandas', 'geopandas', 'cartopy', 'descartes'],
     py_modules=['geoplot', 'crs', 'utils', 'quad'],
     version='0.2.3',
     description='High-level geospatial plotting for Python.',
