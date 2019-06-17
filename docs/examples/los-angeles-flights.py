50206b266fde16e79f3f760c586d0d6d4ff4eea5
ResidentMario <aleksey.bilogur@gmail.com>
2019-06-17 12:40:13 -0700

    Update examples.

--- docs/examples/los-angeles-flights.py
+++ docs/examples/los-angeles-flights.py
@@ -1,24 +1,19 @@
-
 # This script demonstrates using the cartopy feature interface alongside geoplot.
 # For more information visit http://scitools.org.uk/cartopy/docs/latest/matplotlib/feature_interface.html.
 
-
-# Load the data (uses the `quilt` package).
 import geopandas as gpd
-from quilt.data.ResidentMario import geoplot_data
+import geoplot as gplt
+import geoplot.crs as gcrs
+import matplotlib.pyplot as plt
+import cartopy
 
-la_flights = gpd.read_file(geoplot_data.la_flights())
+# load the data
+la_flights = gpd.read_file(gplt.datasets.get_path('la_flights'))
 la_flights = la_flights.assign(
     start=la_flights.geometry.map(lambda mp: mp[0]),
     end=la_flights.geometry.map(lambda mp: mp[1])
 )
 
-
-# Plot the data.
-import geoplot as gplt
-import geoplot.crs as gcrs
-import matplotlib.pyplot as plt
-import cartopy
 
 f, axarr = plt.subplots(2, 2, figsize=(12, 12), subplot_kw={
     'projection': gcrs.Orthographic(central_latitude=40.7128, central_longitude=-74.0059)
--- docs/examples/nyc-collisions-map.py
+++ docs/examples/nyc-collisions-map.py
@@ -1,41 +1,42 @@
-# Load the data (uses the `quilt` package).
 import geopandas as gpd
-from quilt.data.ResidentMario import geoplot_data
-
-boroughs = gpd.read_file(geoplot_data.nyc_boroughs())
-fatal_collisions = gpd.read_file(geoplot_data.nyc_fatal_collisions())
-injurious_collisions = gpd.read_file(geoplot_data.nyc_injurious_collisions())
-
-
-# Plot the data.
 import geoplot as gplt
 import geoplot.crs as gcrs
 import matplotlib.pyplot as plt
 
+# load the data
+nyc_boroughs = gpd.read_file(gplt.datasets.get_path('nyc_boroughs'))
+nyc_fatal_collisions = gpd.read_file(gplt.datasets.get_path('nyc_fatal_collisions'))
+nyc_injurious_collisions = gpd.read_file(gplt.datasets.get_path('nyc_injurious_collisions'))
+
+
 fig = plt.figure(figsize=(10,5))
+proj = projection=gcrs.AlbersEqualArea(central_latitude=40.7128, central_longitude=-74.0059)
+ax1 = plt.subplot(121, projection=proj)
+ax2 = plt.subplot(122, projection=proj)
 
-ax1 = plt.subplot(121, projection=gcrs.AlbersEqualArea(central_latitude=40.7128, central_longitude=-74.0059))
-gplt.polyplot(boroughs, ax=ax1, projection=gcrs.AlbersEqualArea())
-gplt.pointplot(fatal_collisions, projection=gcrs.AlbersEqualArea(),
-               hue='BOROUGH', categorical=True,
-               edgecolor='white', linewidth=0.5, zorder=10,
-               scale='NUMBER OF PERSONS KILLED', limits=(2, 8),
-               legend=True, legend_var='scale', legend_kwargs={'loc': 'upper left'},
-               legend_values=[2, 1], legend_labels=['2 Fatalities', '1 Fatality'],
-               ax=ax1)
-plt.title("Fatal Crashes in New York City, 2016")
+gplt.polyplot(nyc_boroughs, ax=ax1, projection=proj)
+gplt.pointplot(
+    nyc_fatal_collisions, projection=proj,
+    hue='BOROUGH', categorical=True,
+    edgecolor='white', linewidth=0.5, zorder=10,
+    scale='NUMBER OF PERSONS KILLED', limits=(2, 8),
+    legend=True, legend_var='scale', legend_kwargs={'loc': 'upper left'},
+    legend_values=[2, 1], legend_labels=['2 Fatalities', '1 Fatality'],
+    ax=ax1
+)
+ax1.set_title("Fatal Crashes in New York City, 2016")
 
-ax2 = plt.subplot(122, projection=gcrs.AlbersEqualArea(central_latitude=40.7128, central_longitude=-74.0059))
-gplt.polyplot(boroughs, ax=ax2, projection=gcrs.AlbersEqualArea())
-gplt.pointplot(injurious_collisions, projection=gcrs.AlbersEqualArea(),
-               hue='BOROUGH', categorical=True,
-               edgecolor='white', linewidth=0.5, zorder=10,
-               scale='NUMBER OF PERSONS INJURED', limits=(1, 10),
-               legend=True, legend_var='scale', legend_kwargs={'loc': 'upper left'},
-               legend_values=[20, 15, 10, 5, 1],
-               legend_labels=['20 Injuries', '15 Injuries', '10 Injuries', '5 Injuries', '1 Injury'],
-               ax=ax2)
-plt.title("Injurious Crashes in New York City, 2016")
-
+gplt.polyplot(nyc_boroughs, ax=ax2, projection=proj)
+gplt.pointplot(
+    nyc_injurious_collisions, projection=proj,
+    hue='BOROUGH', categorical=True,
+    edgecolor='white', linewidth=0.5, zorder=10,
+    scale='NUMBER OF PERSONS INJURED', limits=(1, 10),
+    legend=True, legend_var='scale', legend_kwargs={'loc': 'upper left'},
+    legend_values=[20, 15, 10, 5, 1],
+    legend_labels=['20 Injuries', '15 Injuries', '10 Injuries', '5 Injuries', '1 Injury'],
+    ax=ax2
+)
+ax2.set_title("Injurious Crashes in New York City, 2016")
 
 plt.savefig("nyc-collisions-map.png", bbox_inches='tight', pad_inches=0)
