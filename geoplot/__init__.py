66e83ecf79a257f053747e49e5d0fc7701fbf48a
ResidentMario <aleksey.bilogur@gmail.com>
2019-06-30 14:00:37 -0400

    Tweak extent-setting to avoid certain clipping issues.

--- geoplot/__init__.py
+++ geoplot/__init__.py
@@ -11,5 +11,4 @@ from .utils import (
     gaussian_points, gaussian_polygons, gaussian_multi_polygons, uniform_random_global_points,
     uniform_random_global_network, classify_clusters
 )
-from .quad import (QuadTree, subpartition, flatten)
 from .datasets import get_path
