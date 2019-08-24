a1c830eea3d987d67cccfc0f9527f61ddcf9028c
ResidentMario <aleksey.bilogur@gmail.com>
2019-08-24 12:16:19 -0700

    Export webmercator and webmap.

--- geoplot/__init__.py
+++ geoplot/__init__.py
@@ -1,11 +1,12 @@
 from .geoplot import (
-    pointplot, polyplot, choropleth, cartogram, kdeplot, sankey, voronoi, quadtree, __version__
+    pointplot, polyplot, choropleth, cartogram, kdeplot, sankey, voronoi, quadtree, webmap,
+    __version__
 )
 from .crs import (
     PlateCarree, LambertCylindrical, Mercator, Miller, Mollweide, Robinson, Sinusoidal,
     InterruptedGoodeHomolosine, Geostationary, NorthPolarStereo, SouthPolarStereo, Gnomonic,
     AlbersEqualArea, AzimuthalEquidistant, LambertConformal, Orthographic, Stereographic,
-    TransverseMercator, LambertAzimuthalEqualArea, UTM, OSGB, EuroPP, OSNI
+    TransverseMercator, LambertAzimuthalEqualArea, UTM, OSGB, EuroPP, OSNI, WebMercator
 )
 from .utils import (
     gaussian_points, gaussian_polygons, gaussian_multi_polygons, uniform_random_global_points,
