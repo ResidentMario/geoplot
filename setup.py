453b5f0b71a6bfdbb3ae586ab765dda0622d8789
Aleksey Bilogur <aleksey@residentmar.io>
2020-05-17 17:47:11 -0400

    Undo temp test version bump.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     ],
     extras_require={'develop': ['pytest', 'pytest-mpl', 'scipy']},
     py_modules=['geoplot', 'crs', 'utils', 'ops'],
-    version='0.4.1',
+    version='0.4.0',
     python_requires='>=3.6.0',
     description='High-level geospatial plotting for Python.',
     author='Aleksey Bilogur',
     author_email='aleksey.bilogur@gmail.com',
     url='https://github.com/ResidentMario/geoplot',
-    download_url='https://github.com/ResidentMario/geoplot/tarball/0.4.1',
+    download_url='https://github.com/ResidentMario/geoplot/tarball/0.4.0',
     keywords=[
         'data', 'data visualization', 'data analysis', 'data science', 'pandas', 'geospatial data',
         'geospatial analytics'
