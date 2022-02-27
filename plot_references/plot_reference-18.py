2cf0ea0d4d95c8322dad0578aee0252712886746
Aleksey Bilogur <aleksey@residentmar.io>
2022-02-27 15:27:38 -0500

    Update docs.

--- plot_references/plot_reference-18.py
+++ plot_references/plot_reference-18.py
@@ -1,3 +1,3 @@
 ax = gplt.polyplot(boroughs, projection=gcrs.AlbersEqualArea(), zorder=1)
-gplt.kdeplot(collisions, cmap='Reds', shade=True, shade_lowest=True,
+gplt.kdeplot(collisions, cmap='Reds', shade=True, thresh=0.05,
              clip=boroughs, ax=ax)
